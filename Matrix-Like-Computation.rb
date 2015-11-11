67ff6c7ae8d3b900973af654c2d314e697ff2962
Atsushi Kano <s1230070@sshsv1.u-aizu.ac.jp>
2015-11-12 05:18:53 +0900

    passed

--- Matrix-Like-Computation.rb
+++ Matrix-Like-Computation.rb
@@ -1,4 +1,3 @@
-# coding: utf-8
 while n = STDIN.gets.to_i do
   break if n == 0
   twodarr = []
@@ -6,8 +5,16 @@ while n = STDIN.gets.to_i do
     arr = STDIN.gets.split.map(&:to_i)
     twodarr[i] = arr
   }
+  n.times{ |i|
+    twodarr[i][twodarr.size] = twodarr[i].inject(:+)
+    (twodarr.size+1).times{ |j|
+      print twodarr[i][j].to_s.rjust(5)
+    }
+    puts
+  }
+  rowsum = twodarr.transpose.map{|ary| ary.inject(&:+)}
+  (twodarr.size+1).times{ |i|
+    print rowsum[i].to_s.rjust(5)
+  }
+  puts
 end
-
-p twodarr
-
-#totyuu
