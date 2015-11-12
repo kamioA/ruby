346d29fb08cac016bfee3870f3cea2a82e510a19
Atsushi Kano <s1230070@stdrsv1122.u-aizu.ac.jp>
2015-11-12 17:51:50 +0900

    fixed

--- Min-Max-And-Sum.rb
+++ Min-Max-And-Sum.rb
@@ -1,3 +1,16 @@
-n = STDIN.gets.to_i
+n = STDIN.gets
 arr = STDIN.gets.split.map(&:to_i)
-print arr.min,' ',arr.max,' ',arr.inject(:+),"\n"
+Min = 1000000
+Max = -1000000
+min = arr.inject(Min){|min,dum|  if dum > min then
+                           min
+                           else
+                           dum
+                           end}
+
+max = arr.inject(Max){|max,dum| if dum < max then
+                          max
+                          else
+                          dum
+                          end}
+print min,' ',max,' ',arr.inject(:+),"\n"
